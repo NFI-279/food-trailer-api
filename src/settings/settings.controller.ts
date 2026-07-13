d1219724beaa830b8feb957267c07d0998f31583
Niculae Filip Ioan <filip_ioan.niculae@stud.fils.upb.ro>
2026-07-13 11:44:08 +0300

    Add Public decorator to getSettings endpoint

--- src/settings/settings.controller.ts
+++ src/settings/settings.controller.ts
@@ -1,11 +1,13 @@
 // src/settings/settings.controller.ts
 import { Controller, Get, Patch, Body } from '@nestjs/common';
 import { SettingsService } from './settings.service';
+import { Public } from '../auth/public.decorator';
 
 @Controller('settings')
 export class SettingsController {
   constructor(private readonly settingsService: SettingsService) {}
-
+  
+  @Public()
   @Get()
   getSettings() {
     return this.settingsService.getSettings();
