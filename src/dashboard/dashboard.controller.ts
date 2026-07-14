88a41c6904c1277b2faac70c0f02eab27696b9d3
Niculae Filip Ioan <filip_ioan.niculae@stud.fils.upb.ro>
2026-07-14 11:56:30 +0300

    Update dashboard.controller.ts

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,16 +1,19 @@
 // src/dashboard/dashboard.controller.ts
 import { Controller, Get } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
+import { Roles } from '../auth/roles.decorator';
 
 @Controller('dashboard')
 export class DashboardController {
   constructor(private readonly dashboardService: DashboardService) {}
 
+   @Roles('ADMIN')
   @Get()
   getStats() {
     return this.dashboardService.getStats();
   }
 
+   @Roles('ADMIN')
   @Get('chart')
   getAnalytics() {
     return this.dashboardService.getAnalytics();
