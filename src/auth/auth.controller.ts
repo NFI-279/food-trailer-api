eaa67b0030ed5f0242a256f25ccc6e55e50160d3
Niculae Filip Ioan <filip_ioan.niculae@stud.fils.upb.ro>
2026-08-10 02:27:50 +0300

    Disable password change functionality
    Prevent password changes in demo environment.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 // src/auth/auth.controller.ts
-import { Controller, Post, Body, Request, Patch } from '@nestjs/common';
+import { Controller, Post, Body, Request, Patch, ForbiddenException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Public } from './public.decorator';
 
@@ -15,6 +15,8 @@ export class AuthController {
   @Patch('password')
   changePassword(@Request() req, @Body() body: Record<string, string>) {
     // req.user comes from our JwtAuthGuard!
-    return this.authService.changePassword(req.user.sub, body.oldPassword, body.newPassword);
+
+    throw new ForbiddenException('Password changes are disabled in this demo environment.');
+    //return this.authService.changePassword(req.user.sub, body.oldPassword, body.newPassword);
   }
 }
